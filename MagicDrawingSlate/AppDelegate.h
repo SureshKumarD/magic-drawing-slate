06a47ae52115a963ac50775abe8fb16258c6cf41
Suresh Kumar Durairaj <sureshkumar_durairaj@yahoo.co.in>
2016-12-05 13:41:03 +0530

    Initial Commit

---
+++ MagicDrawingSlate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagicDrawingSlate
+//
+//  Created by Suresh on 12/5/16.
+//  Copyright © 2016 Suresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MagicDrawingSlate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagicDrawingSlate
+//
+//  Created by Suresh on 12/5/16.
+//  Copyright © 2016 Suresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
